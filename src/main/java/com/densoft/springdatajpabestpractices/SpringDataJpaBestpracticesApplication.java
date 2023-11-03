004160fdd6b1e5525f2a5422049b60b48c7f1f7e
dennis githinji <wakahiad@gmail.com>
2023-11-03 14:01:57 +0300

    project setup

---
+++ src/main/java/com/densoft/springdatajpabestpractices/SpringDataJpaBestpracticesApplication.java
@@ -0,0 +1,13 @@
+package com.densoft.springdatajpabestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaBestpracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaBestpracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/densoft/springdatajpabestpractices/SpringDataJpaBestpracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.densoft.springdatajpabestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaBestpracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
