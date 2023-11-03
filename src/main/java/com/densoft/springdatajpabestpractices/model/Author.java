791d76ac3a81c07dfaf78ff4daf9b108bba74b16
dennis githinji <wakahiad@gmail.com>
2023-11-03 17:41:55 +0300

    How to effectively shape the @OneToMany Association

---
+++ src/main/java/com/densoft/springdatajpabestpractices/model/Author.java
@@ -0,0 +1,54 @@
+package com.densoft.springdatajpabestpractices.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Author implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String genre;
+    private int age;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "author", orphanRemoval = true)
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        this.books.add(book);
+        book.setAuthor(this);
+    }
+    public void removeBook(Book book) {
+        book.setAuthor(null);
+        this.books.remove(book);
+    }
+    public void removeBooks() {
+        Iterator<Book> iterator = this.books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            book.setAuthor(null);
+            iterator.remove();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "id=" + id +
+                ", name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/densoft/springdatajpabestpractices/model/Book.java
@@ -0,0 +1,52 @@
+package com.densoft.springdatajpabestpractices.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+public class Book implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 2021;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+}
